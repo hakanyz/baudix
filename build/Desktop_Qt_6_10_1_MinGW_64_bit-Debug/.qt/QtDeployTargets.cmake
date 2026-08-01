0d134c368c4d1bb5bfe18a8a8833b4729e5bf119
Hakan <hakanyz@outlook.com>
2026-08-02 02:42:09 +0300

    chore: remove build directory from git tracking and update gitignore

--- build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_baudix_FILE C:/Users/hakan/OneDrive/Desktop/hakan_workspace/hakan/baudix/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/baudix.exe)
-set(__QT_DEPLOY_TARGET_baudix_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_baudix_RUNTIME_DLLS C:/Qt/6.10.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Core.dll)
